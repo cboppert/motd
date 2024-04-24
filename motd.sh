2044dcefbd787a1b5ca30362eaecb1a62611e866
Cody Boppert <cody@codyboppert.nyc>
2024-04-23 21:34:04 -0400

    Delete tmux tips for now since not much interesting to add

--- motd.sh
+++ motd.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 source $(dirname $0)/vim-tips.sh
-source $(dirname $0)/tmux-tips.sh
 source $(dirname $0)/regex-tips.sh
 source $(dirname $0)/styler.sh
 
 
-TIPS=("${VIM_TIPS[@]}" "${TMUX_TIPS[@]}" "${REGEX_TIPS[@]}")
+TIPS=("${VIM_TIPS[@]}" "${REGEX_TIPS[@]}")
 # TIPS=("${REGEX_TIPS[@]}")
 
 RAND=$[$RANDOM % ${#TIPS[@]}]
